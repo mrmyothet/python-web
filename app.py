ea90c8654cd61e6ef92383047ee4a8b9cbd4a853
Myo Thet <mr.myothet@gmail.com>
2024-08-10 11:10:13 +0700

    operators - addition, subtraction, multiplication, division

---
+++ app.py
@@ -0,0 +1,20 @@
+name = 'John'
+age = 12
+city = 'Yangon'
+religion = 'Buddhist'
+birth_year = 2012
+has_drivingLicense = True
+
+print(name)
+print(age)
+print(religion)
+print(birth_year)
+
+type_of_name = type(name)
+print(type_of_name)
+
+type_of_age = type(age)
+print(type_of_age)
+
+type_of_has_drivingLicense = type(has_drivingLicense)
+print(type_of_has_drivingLicense)
---
+++ operators.py
@@ -0,0 +1,32 @@
+# arithmetic operators -> binary operators
+# comparison operators -> binary operators
+# logical operators (and or not)
+# assignment operators 
+
+# addition 
+print('addition')
+num1 = 10
+num2 = 12
+result = num1 + num2
+print(result)
+
+# subtraction
+
+# multiplication 
+print('multiplication')
+num1 = '2'
+num2 = 4
+result = num1 * num2
+print(result)
+
+
+# division
+print('division')
+# floor division    => // 
+# float division    => /
+# modulus division  => %
+num1 = 5
+num2 = 2
+print('floor division - ', '5/2', '=>', 5//2)
+print('float division - ', '5/2', '=>', 5/2)
+print('modulus division - ', '5/2', '=>', 5%2)
