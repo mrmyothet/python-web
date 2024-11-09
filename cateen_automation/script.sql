213f6930d8abaa86424b43fff4358eb82123a674
Myo Thet <mr.myothet@gmail.com>
2024-11-09 14:41:02 +0700

    cateen automation - database script

---
+++ cateen_automation/script.sql
@@ -0,0 +1,17 @@
+-- Database: canteen_automation
+
+-- DROP DATABASE IF EXISTS canteen_automation;
+
+CREATE DATABASE canteen_automation
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+use canteen_automation;
+
