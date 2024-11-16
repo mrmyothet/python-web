d8fc3cf0efb7e2924eec8135541492a680d5ce3b
Myo Thet <mr.myothet@gmail.com>
2024-11-16 10:55:26 +0700

    read from the database using psycopg2

--- canteen_automation/connect_db_windows.py
+++ canteen_automation/connect_db_windows.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 conn = psycopg2.connect(
-    dbname="cateen_automation",
+    dbname="canteen_automation",
     user="postgres",
     password="admin123!",
     host="127.0.0.1",
@@ -10,6 +10,14 @@ conn = psycopg2.connect(
 
 print(conn)
 
+# cursor = conn.cursor()
+# cursor.execute("INSERT INTO roles (role_name) VALUES ('Staff')")
+# conn.commit()
+
 cursor = conn.cursor()
-cursor.execute("INSERT INTO roles (role_name) VALUES ('Staff')")
-conn.commit()
+query = "SELECT * FROM roles"
+lst = cursor.execute(query)
+rows = cursor.fetchall()
+# conn.commit()
+
+print(rows)
