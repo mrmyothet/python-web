1c9153c6d5ca965e7f7a09b02eafb2a494b901f6
Myo Thet <mr.myothet@gmail.com>
2024-12-07 12:28:24 +0700

    Blog - connect to postgres server 16

---
+++ Blogs/app/db.py
@@ -0,0 +1,13 @@
+import psycopg2
+from psycopg2 import sql
+
+
+def get_db():
+    conn = psycopg2.connect(
+        dbname="Blog",
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="5434",
+    )
+    return conn
